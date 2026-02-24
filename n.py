391d38e73802a95e5e8c6847acd222b7d70f1972
Kashif Raza <39432246+kashifraza01@users.noreply.github.com>
2026-02-24 14:42:49 +0500

    practice

---
+++ n.py
@@ -0,0 +1,16 @@
+zz=input("enter your name: ")
+# has_vowel=False
+# for ch in zz:
+#     if ch.lower() in "aeiou":
+#         has_vowel=True
+#         break
+# if has_vowel:
+#     print("has vowels")
+# else:
+#     print("doesn't have vowels")
+
+if any(ch in "AEIOUaeiou" for ch in zz):
+    print("has vowels")
+else:
+    print("doesn't has vowels")
+    
